fd620afa530ca19c6b5ffb011a68915a9c4eb8b7
Manas-Singh214 <manassingh2142006@gmail.com>
2026-04-01 21:56:51 +0530

    d

--- run_backend.bat
+++ run_backend.bat
@@ -1,6 +1,7 @@
 @echo off
 echo Starting ATMOSCHAIN WORK Backend...
 cd /d "%~dp0backend"
+call ..\.venv\Scripts\activate
 pip install -r requirements.txt -q
 echo.
 echo Backend running at http://localhost:8000
